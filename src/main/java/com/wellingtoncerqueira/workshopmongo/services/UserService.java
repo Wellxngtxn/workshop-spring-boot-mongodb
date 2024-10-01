92976f50921da836975aa3da39f54a89acc77d56
Wellington Cerqueira <Wellingtoncerqueira17@gmail.com>
2024-10-01 15:46:36 -0300

    Obtendo um usuario por ID

--- src/main/java/com/wellingtoncerqueira/workshopmongo/services/UserService.java
+++ src/main/java/com/wellingtoncerqueira/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.wellingtoncerqueira.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wellingtoncerqueira.workshopmongo.domain.User;
 import com.wellingtoncerqueira.workshopmongo.repository.UserRepository;
+import com.wellingtoncerqueira.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User FindById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
