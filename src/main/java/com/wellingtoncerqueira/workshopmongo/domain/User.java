8cb0667bd924d57bebff679ef1327b347aa6602a
Wellington Cerqueira <Wellingtoncerqueira17@gmail.com>
2024-10-02 16:27:21 -0300

    Referenciando os posts do usuario

--- src/main/java/com/wellingtoncerqueira/workshopmongo/domain/User.java
+++ src/main/java/com/wellingtoncerqueira/workshopmongo/domain/User.java
@@ -1,10 +1,14 @@
 package com.wellingtoncerqueira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection="user")
 public class User implements Serializable {
@@ -15,6 +19,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 
@@ -55,6 +62,14 @@ public class User implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -67,6 +82,9 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
+	
 	
 	
 }
